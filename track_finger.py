4bff7fa2069e2e11edcd64cc0f49d6cfd544466d
sho7328 <sophie.ho@menloschool.org>
2024-04-24 17:23:22 -0700

    integrated pygame and opencv

--- track_finger.py
+++ track_finger.py
@@ -2,6 +2,7 @@ import cv2
 import mediapipe as mp
 from mediapipe import solutions
 from mediapipe.framework.formats import landmark_pb2
+import pygame
 
 # Library Constants
 BaseOptions = mp.tasks.BaseOptions
@@ -11,9 +12,118 @@ HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
 VisionRunningMode = mp.tasks.vision.RunningMode
 DrawingUtil = mp.solutions.drawing_utils
 
-# Create the hand detector
-base_options = BaseOptions(model_asset_path='data/hand_landmarker.task')
-options = HandLandmarkerOptions(base_options=base_options, num_hands=2)
-detector = HandLandmarker.create_from_options(options)
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
+
+class Track_hand:
+    def __init__(self):
+        # Load game elements
+        self.score = 0
+
+
+        # Create the hand detector
+        base_options = BaseOptions(model_asset_path='data/hand_landmarker.task')
+        options = HandLandmarkerOptions(base_options=base_options,
+                                                num_hands=2)
+        self.detector = HandLandmarker.create_from_options(options)
+
+        # TODO: Load video
+        self.video = cv2.VideoCapture(0)
+
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption("Test")
+
+        # Load background image
+        self.background = pygame.image.load("background_image.png")
+        self.background = pygame.transform.scale(self.background, (SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
+    def get_finger_position(self, image, detection_result):
+        """
+        Draws a green circle on the index finger 
+        and calls a method to check if we've intercepted
+        with the enemy
+        Args:
+            image (Image): The image to draw on
+            detection_result (HandLandmarkerResult): HandLandmarker detection results
+        """
+        # Get image details
+        imageHeight, imageWidth = image.shape[:2]
+
+        # Get a list of the landmarks
+        hand_landmarks_list = detection_result.hand_landmarks
+        
+        # Loop through the detected hands to visualize.
+        for idx in range(len(hand_landmarks_list)):
+            hand_landmarks = hand_landmarks_list[idx]
+
+            # get the coordinates of just teh index finger
+            finger = hand_landmarks[HandLandmarkPoints.INDEX_FINGER_TIP.value]
+
+
+            # map the coordinates back to screen dimensions
+            pixelCoord = DrawingUtil._normalized_to_pixel_coordinates(finger.x, finger.y, imageWidth, imageHeight)
+            
+            return pixelCoord
+            # cv2.circle(image, (pixelCoord[0], pixelCoord[1]), 25, GREEN, 5
+            
+    def run(self):
+        """
+        Main game loop. Runs until the 
+        user presses "q".
+        """    
+
+        running = True
+        # TODO: Modify loop condition  
+        while self.video.isOpened() and running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    break
+                    
+            # Get the current frame
+            frame = self.video.read()[1]
+
+            # Convert it to an RGB image
+            image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            # the image comes mirroed-flip it
+            image = cv2.flip(image, 1)
+
+            # Convert the image to a readable format and find the hands
+            to_detect = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
+            results = self.detector.detect(to_detect)
+
+
+            # Draw the hand landmarks
+            # self.draw_landmarks_on_hand(image, results)
+            pixelCoord = self.get_finger_position(image, results)
+
+                # Draw background
+            self.screen.blit(self.background, (0, 0))
+
+            pygame.draw.circle(self.background, (0, 255, 0), [pixelCoord[0], pixelCoord[1]], 15, 3)
+            
+            # Draws the surface object to the screen.
+            pygame.display.update()
+
+            # Change the color of the frame back
+            # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            # cv2.imshow('Hand Tracking', image)
+
+            # Break the loop if the user presses 'q'
+            if cv2.waitKey(50) & 0xFF == ord('q'):
+                print(self.score)
+                break
+                
+        self.video.release()
+        cv2.destroyAllWindows()
+
+
+
+
+
+
 
 
